376ba550c3877747a42713faf621e2e774cb0af8
Pavel Mazhuga <pavelmazhuga@yandex.ru>
2023-04-26 08:10:20 +0700

    useScroll instead of useWheel

---
+++ src/components/layout/WebGL/CameraMovement.tsx
@@ -0,0 +1,36 @@
+import { useScroll } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
+import { RefObject, useRef } from 'react';
+import { Light, Object3D, Vector3 } from 'three';
+import { lerp } from '@/utils/lerp';
+
+type Props = {
+    light: RefObject<Light>;
+};
+
+const CameraMovement = ({ light }: Props) => {
+    const cameraLookAtObject = useRef<Object3D>(null);
+    const cameraLookAtObjectPosition = useRef(new Vector3());
+    const scroll = useScroll();
+
+    useFrame(({ camera }) => {
+        camera.position.x = lerp(camera.position.x, scroll.offset * scroll.pages * 8.5, 0.07);
+
+        cameraLookAtObjectPosition.current.x = camera.position.x;
+        cameraLookAtObjectPosition.current.y = 2;
+        cameraLookAtObjectPosition.current.z = 0;
+
+        if (cameraLookAtObject.current) {
+            cameraLookAtObject.current.position.lerp(cameraLookAtObjectPosition.current, 0.08);
+            camera.lookAt(cameraLookAtObject.current.position);
+        }
+
+        if (light.current) {
+            light.current.position.x = lerp(light.current.position.x, camera.position.x, 0.02);
+        }
+    });
+
+    return <object3D ref={cameraLookAtObject} />;
+};
+
+export default CameraMovement;
--- src/components/layout/WebGL/MainScene.tsx
+++ src/components/layout/WebGL/MainScene.tsx
@@ -1,16 +1,14 @@
 'use client';
 
-import { useFrame } from '@react-three/fiber';
 import { Suspense, useEffect, useMemo, useRef, useState } from 'react';
-import { BoxGeometry, Group, MeshBasicMaterial, Object3D, PointLight, Vector3 } from 'three';
+import { BoxGeometry, Group, MeshBasicMaterial, Object3D, PointLight } from 'three';
 import { useMapRefs } from '@/hooks/use-map-refs';
-import { clamp } from '@/utils/clamp';
-import { lerp } from '@/utils/lerp';
 import Ground from './Ground';
 import Stand from './Stand';
 import Walls from './Walls';
-import { Preload, usePerformanceMonitor } from '@react-three/drei';
+import { Preload, ScrollControls } from '@react-three/drei';
 import { useMediaQueryDeviceState } from '@/atoms/media-query-device';
+import CameraMovement from './CameraMovement';
 
 const portfolio = [
     {
@@ -78,74 +76,8 @@ const portfolio = [
     },
 ];
 
-const useWheel = () => {
-    const ONE_STAND_SCROLL_LENGTH = 2860;
-    const startTouchX = useRef<number>(0);
-    const startTouchY = useRef<number>(0);
-    const prevDeltaX = useRef<number>(0);
-    const prevDeltaY = useRef<number>(0);
-    const wheelX = useRef<number>(0);
-    const wheelY = useRef<number>(0);
-
-    useEffect(() => {
-        const onWheel = (event: WheelEvent) => {
-            const delta = (event.deltaY === 0 ? event.deltaX : event.deltaY) * 2;
-            wheelX.current = clamp(wheelX.current + delta, 0, ONE_STAND_SCROLL_LENGTH * portfolio.length);
-        };
-
-        window.addEventListener('wheel', onWheel);
-
-        return () => window.removeEventListener('wheel', onWheel);
-    }, []);
-
-    useEffect(() => {
-        const onTouchstart = (event: TouchEvent) => {
-            startTouchX.current = event.touches[0].clientX;
-            startTouchY.current = event.touches[0].clientY;
-        };
-
-        const hasScrollDirectionChanged = (prevY: number, currentY: number) => {
-            if (prevDeltaY.current > 0) {
-                return currentY < prevY;
-            } else if (prevDeltaY.current < 0) {
-                return currentY > prevY;
-            }
-
-            return false;
-        };
-
-        const onTouchmove = (event: TouchEvent) => {
-            let deltaX = startTouchX.current - event.touches[0].clientX;
-            let deltaY = startTouchY.current - event.touches[0].clientY;
-
-            if (hasScrollDirectionChanged(prevDeltaY.current, deltaY)) {
-                deltaX = 0;
-                deltaY = 0;
-                startTouchX.current = event.touches[0].clientX;
-                startTouchY.current = event.touches[0].clientY;
-            }
-
-            const delta = (Math.abs(deltaX) > Math.abs(deltaY) ? deltaX : deltaY) * 2;
-            wheelX.current = clamp(wheelX.current + delta, 0, ONE_STAND_SCROLL_LENGTH * portfolio.length);
-            prevDeltaX.current = deltaX;
-            prevDeltaY.current = deltaY;
-        };
-
-        window.addEventListener('touchstart', onTouchstart);
-        window.addEventListener('touchmove', onTouchmove);
-
-        return () => {
-            window.removeEventListener('touchstart', onTouchstart);
-            window.removeEventListener('touchmove', onTouchmove);
-        };
-    }, []);
-
-    return { wheelX, wheelY };
-};
-
 const MainScene = () => {
     const projectStandRefs = useMapRefs<Group>(portfolio);
-    const { wheelX } = useWheel();
     const [hoveredStandIndex, setHoveredStandIndex] = useState<number | null>(null);
     const standWidth = 4.5;
     const standHeight = 8;
@@ -154,43 +86,7 @@ const MainScene = () => {
     // const standMaterial = useMemo(() => new MeshStandardMaterial({ color: '#cbcbcb' }), []);
 
     const pointLight = useRef<PointLight>(null);
-    const cameraLookAtObject = useRef<Object3D>(null);
-    const cameraLookAtObjectPosition = useRef(new Vector3());
-    const [groundHidden, setGroundHidden] = useState(false);
     const [mediaQueryDevice] = useMediaQueryDeviceState();
-
-    usePerformanceMonitor({
-        onChange: ({ factor }) => {
-            setGroundHidden(factor < 0.4);
-        },
-    });
-
-    useFrame(({ camera, pointer }) => {
-        camera.position.x = lerp(camera.position.x, wheelX.current * 0.003, 0.07);
-
-        // if (
-        //     typeof hoveredStandIndex === 'number' &&
-        //     projectStandRefs.current[hoveredStandIndex].current instanceof Mesh
-        // ) {
-        //     cameraLookAtObjectPosition.current.x = projectStandRefs.current[hoveredStandIndex].current!.position.x;
-        // } else {
-        cameraLookAtObjectPosition.current.x = camera.position.x;
-        cameraLookAtObjectPosition.current.y = 2;
-        cameraLookAtObjectPosition.current.z = 0;
-        // }
-
-        if (cameraLookAtObject.current) {
-            cameraLookAtObject.current.position.lerp(cameraLookAtObjectPosition.current, 0.08);
-            camera.lookAt(cameraLookAtObject.current.position);
-        }
-
-        if (pointLight.current) {
-            pointLight.current.position.x = lerp(pointLight.current.position.x, camera.position.x, 0.02);
-        }
-
-        // camera.position.z = lerp(camera.position.z, typeof hoveredStandIndex === 'number' ? 15 : 20, 0.02);
-        // camera.rotation.y = lerp(camera.rotation.y, typeof hoveredStandIndex === 'number' ? 0.1 : 0, 0.02);
-    });
 
     return (
         <>
@@ -198,12 +94,15 @@ const MainScene = () => {
                 <Suspense>
                     <Walls />
                 </Suspense>
-                {!groundHidden && mediaQueryDevice === 'desktop' && (
+                {mediaQueryDevice === 'desktop' && (
                     <Suspense>
                         <Ground />
                     </Suspense>
                 )}
             </Suspense>
+            <ScrollControls horizontal damping={0} pages={portfolio.length} distance={0.45}>
+                <CameraMovement light={pointLight} />
+            </ScrollControls>
 
             {portfolio.map((project, i) => (
                 <Stand
@@ -230,12 +129,7 @@ const MainScene = () => {
                 />
             ))}
 
-            <object3D ref={cameraLookAtObject} />
-
-            {/* {mediaQueryDevice === 'desktop' && ( */}
             <pointLight ref={pointLight} position={[30, 55, -8]} color="#f5f5f5" intensity={0.62} distance={73} />
-            {/* )} */}
-
             <Preload all />
         </>
     );
