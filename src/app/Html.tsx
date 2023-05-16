33fa78e7e5f1b5edb7c7e86923cf10aea7f3c959
Pavel Mazhuga <pavelmazhuga@yandex.ru>
2023-05-16 07:02:52 +0700

    add metrics

--- src/app/Html.tsx
+++ src/app/Html.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { ReactNode } from 'react';
+import Script from 'next/script';
 import { useMediaQueryDeviceState } from '@/atoms/media-query-device';
 import useIsomorphicLayoutEffect from '@/hooks/use-isomorphic-layout-effect';
 import { calculateScrollbarWidth } from '@/utils/calculate-scrollbar-width';
@@ -10,6 +11,8 @@ if (typeof window !== 'undefined') {
     vhMobileFix();
     calculateScrollbarWidth();
 }
+
+const YM_ID = 93587310;
 
 const Html = ({ children }: { children: ReactNode }) => {
     const [_, setMediaQueryDeviceState] = useMediaQueryDeviceState();
@@ -45,7 +48,37 @@ const Html = ({ children }: { children: ReactNode }) => {
         return () => window.removeEventListener('resize', setDevice);
     }, [setMediaQueryDeviceState]);
 
-    return <html lang="en">{children}</html>;
+    return (
+        <html lang="en">
+            {process.env.NODE_ENV === 'production' && (
+                <Script
+                    id="yandex_metrika"
+                    strategy="beforeInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                    <!-- Yandex.Metrika counter -->
+                    <script type="text/javascript" >
+                       (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                       m[i].l=1*new Date();
+                       for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                       k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                       (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+                    
+                       ym(${YM_ID}, "init", {
+                            clickmap:true,
+                            trackLinks:true,
+                            accurateTrackBounce:true
+                       });
+                    </script>
+                    <noscript><div><img src="https://mc.yandex.ru/watch/93587310" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+                    <!-- /Yandex.Metrika counter -->
+                `,
+                    }}
+                />
+            )}
+            {children}
+        </html>
+    );
 };
 
 export default Html;
