4c0a2f03585a31965dc788e516fec484400acc3e
Pavel Mazhuga <pavelmazhuga@yandex.ru>
2023-07-15 10:05:19 +0700

    .

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withBundleAnalyzer({
-    output: 'export',
     reactStrictMode: true,
     // typescript: {
     //     // !! WARN !!
