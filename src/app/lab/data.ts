920dd6eb32b96802d5a7ee171485155da90570fb
Pavel Mazhuga <pavelmazhuga@yandex.ru>
2023-08-06 07:39:38 +0700

    tags

--- src/app/lab/data.ts
+++ src/app/lab/data.ts
@@ -3,8 +3,8 @@ import planeWavePreview from '@/app/lab/plane-wave/preview.jpeg';
 import displacedTorusPreview from '@/app/lab/displaced-torus/preview.jpeg';
 import animatedBlobPreview from '@/app/lab/animated-blob/preview.jpeg';
 
-export const experiments: { name: string; slug: string; preview?: ImageShape }[] = [
-    { name: 'Plane wave', slug: 'plane-wave', preview: planeWavePreview },
-    { name: 'Displaced torus', slug: 'displaced-torus', preview: displacedTorusPreview },
-    { name: 'Animated blob', slug: 'animated-blob', preview: animatedBlobPreview },
+export const experiments: { name: string; slug: string; tags: string[]; preview?: ImageShape }[] = [
+    { name: 'Plane wave', slug: 'plane-wave', tags: ['webgl', 'shaders'], preview: planeWavePreview },
+    { name: 'Displaced torus', slug: 'displaced-torus', tags: ['webgl', 'shaders'], preview: displacedTorusPreview },
+    { name: 'Animated blob', slug: 'animated-blob', tags: ['webgl', 'shaders'], preview: animatedBlobPreview },
 ];
