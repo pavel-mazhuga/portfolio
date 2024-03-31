bdc57f75f0af8a53342741c05675394fb36d862d
Pavel Mazhuga <pavelmazhuga@yandex.ru>
2024-03-31 14:50:57 +0700

    .

--- src/app/lab/particles-photo-mouse-trail/experience.tsx
+++ src/app/lab/particles-photo-mouse-trail/experience.tsx
@@ -233,7 +233,7 @@ const Experiment = () => {
 
 const Experience = () => {
     return (
-        <ExperimentLayout>
+        <ExperimentLayout sourceLink="https://github.com/pavel-mazhuga/portfolio/tree/main/src/app/lab/particles-photo-mouse-trail">
             <LevaWrapper />
             <div className="canvas-wrapper">
                 <Canvas
