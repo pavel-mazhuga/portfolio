bcb0871e673343773219ffc42ed79f731cc76cb3
Pavel Mazhuga <pavelmazhuga@yandex.ru>
2024-06-10 08:01:42 +0700

    fix

--- src/app/lab/sword/Effects.tsx
+++ src/app/lab/sword/Effects.tsx
@@ -2,7 +2,7 @@ import { Bloom, EffectComposer } from '@react-three/postprocessing';
 
 const Effects = () => {
     return (
-        <EffectComposer disableNormalPass>
+        <EffectComposer>
             <Bloom mipmapBlur luminanceThreshold={0.5} luminanceSmoothing={0.1} intensity={2} />
         </EffectComposer>
     );
