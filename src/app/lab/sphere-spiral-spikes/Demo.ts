823f4c3c3bcb308d55635dff0ffa4a86c9385527
Pavel Mazhuga <pavelmazhuga@yandex.ru>
2026-01-15 15:01:46 +0700

    spiral spikes

--- src/app/lab/sphere-spiral-spikes/Demo.ts
+++ src/app/lab/sphere-spiral-spikes/Demo.ts
@@ -1,5 +1,19 @@
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { Fn, PI2, atan2, cos, normalLocal, positionLocal, select, sin, smoothstep, time, vec3 } from 'three/tsl';
+import {
+    Fn,
+    PI2,
+    atan2,
+    cos,
+    mix,
+    normalLocal,
+    positionLocal,
+    select,
+    sin,
+    smoothstep,
+    time,
+    varyingProperty,
+    vec3,
+} from 'three/tsl';
 import {
     AmbientLight,
     DirectionalLight,
@@ -21,11 +35,15 @@ class SphereSpiralSpikes extends BaseExperience {
         this.controls = new OrbitControls(this.camera, this.canvas);
         this.controls.enableDamping = true;
 
+        const radius = 0.7;
+
         const material = new MeshStandardNodeMaterial({
             color: 0xffaa00,
             roughness: 0.5,
             metalness: 0,
         });
+
+        // const vPosition = varyingProperty('vec3');
 
         const getDisplacedPosition = Fn(([pos]: [any]) => {
             const p = pos.normalize();
@@ -36,19 +54,20 @@ class SphereSpiralSpikes extends BaseExperience {
             const angle = atan2(p.x, p.z);
 
             // Формула спирали
-            const spiralAngle = angle.add(dist.mul(3.0)).sub(time.mul(0));
+            const spiralAngle = angle.add(dist.mul(1.0)).sub(time.mul(0));
             const branches = 6;
 
             const mask = cos(spiralAngle.mul(branches))
-                .smoothstep(0.7, 1.0)
-                .pow(2.0)
-                .mul(dist.smoothstep(0.0, 0.2))
+                .smoothstep(0.5, 1.0)
+                .pow(4.0)
+                .mul(dist.smoothstep(0.0, 0.7))
                 .mul(p.y.add(1.0).smoothstep(0.0, 0.5));
 
             return pos.add(p.mul(mask).mul(0.05));
         });
 
         const displacedPos = getDisplacedPosition(positionLocal).toVar();
+        // vPosition.assign(displacedPos);
         material.positionNode = displacedPos;
 
         // Пересчет нормалей на основе соседей
@@ -68,23 +87,31 @@ class SphereSpiralSpikes extends BaseExperience {
 
         material.normalNode = posT.sub(displacedPos).cross(posB.sub(displacedPos)).normalize();
 
-        // Маска для цвета (рассчитываем один раз для текущей позиции)
-        const mask = getDisplacedPosition(positionLocal).sub(positionLocal).length().div(0.05).toVar();
+        // // Маска для цвета (рассчитываем один раз для текущей позиции)
+        // const mask = getDisplacedPosition(positionLocal).sub(positionLocal).length().div(0.05).toVar();
 
         // Золотистый цвет сферы и белое свечение спиралей
         const baseColor = vec3(1.0, 0.7, 0.1);
-        material.colorNode = baseColor;
+
+        let color = baseColor.toVar('color');
+        color = mix(color, vec3(1, 1, 1), displacedPos.sub(positionLocal).length().div(0.05).toVar('mask'));
+
+        material.colorNode = color;
         // material.emissiveNode = baseColor.mul(mask).mul(1);
 
-        const mesh = new Mesh(new IcosahedronGeometry(0.7, 256), material);
+        const mesh = new Mesh(new IcosahedronGeometry(radius, 256), material);
         this.scene.add(mesh);
 
-        const ambientLight = new AmbientLight(0xffffff, 0.2);
+        const ambientLight = new AmbientLight(0xffffff, 0.3);
         this.scene.add(ambientLight);
 
         const directionalLight = new DirectionalLight(0xffffff, 1);
         directionalLight.position.set(1, 1, 1);
         this.scene.add(directionalLight);
+
+        const directionalLight2 = new DirectionalLight(0xffffff, 1);
+        directionalLight2.position.set(-1, -1, -1);
+        this.scene.add(directionalLight2);
 
         this.initTweakPane();
     }
