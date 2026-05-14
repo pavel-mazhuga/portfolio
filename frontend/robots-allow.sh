d425c072e4e34ae94ef2965e374bef48f9975721
Pavel Mazhuga <pavelmazhuga@yandex.ru>
2026-05-14 10:01:57 +0700

    ci: robots

---
+++ frontend/robots-allow.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "User-Agent: *" > robots.txt
+echo "Allow: *" >> robots.txt
+echo "Disallow: */?" >> robots.txt
+echo "Disallow: */&" >> robots.txt
+echo "Disallow: /admin" >> robots.txt
+echo "Disallow: /file=" >> robots.txt
+echo "Disallow: /*.pdf" >> robots.txt
+
+echo "User-Agent: Yandex" > robots.txt
+echo "Allow: *" >> robots.txt
+echo "Disallow: */?" >> robots.txt
+echo "Disallow: */&" >> robots.txt
+echo "Disallow: /admin" >> robots.txt
+echo "Disallow: /file=" >> robots.txt
+echo "Disallow: /*.pdf" >> robots.txt
+
+echo "Sitemap: https://pavelmazhuga.com/sitemap.xml" >> robots.txt
+
+mv robots.txt build/robots.txt
